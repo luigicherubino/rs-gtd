584d4f23858fc72f8f139248c036faeecc60a528
Derick Schoonbee <derick.schoonbee@gmail.com>
2012-06-30 23:19:34 +0200

    Working on layout

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 #Run the application
 #expect an evironment variable:
-#export RS_ALPHA=~/projects/realsma-build-desktop/bin/rsAlphaServer.app/Contents/MacOS/rsAlphaServer
+export RS_ALPHA=~/projects/realsma-build-desktop/bin/rsAlphaServer.app/Contents/MacOS/rsAlphaServer
 $RS_ALPHA --config conf/RealSMA.conf.xml
